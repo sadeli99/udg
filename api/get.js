45ae0090eece6a6c991718fa8e9ade7801c34a84
Sadeli Ali <152187061+sadeli99@users.noreply.github.com>
2025-07-06 06:19:42 +0700

    Update get.js

--- api/get.js
+++ api/get.js
@@ -3,13 +3,25 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   const { GITHUB_TOKEN, GITHUB_OWNER, GITHUB_REPO } = process.env;
   const { path } = req.query;
+
   if (!path) return res.status(400).json({ error: "Missing path" });
 
   const apiURL = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${path}`;
 
   try {
-    const ghRes = await fetch(apiURL, { headers: { Authorization: `Bearer ${GITHUB_TOKEN}` } });
+    const ghRes = await fetch(apiURL, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+        Accept: "application/vnd.github+json"
+      }
+    });
     const data = await ghRes.json();
+
+    // Jika file tidak ditemukan
+    if (data.message === "Not Found") {
+      return res.status(404).json({ error: "File not found" });
+    }
+
     const content = Buffer.from(data.content, 'base64').toString();
     res.status(200).json({ content: JSON.parse(content), sha: data.sha });
   } catch (err) {
