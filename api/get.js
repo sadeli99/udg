7670ea3fbfd164d70354cc665ceeb135376ded91
Sadeli Ali <152187061+sadeli99@users.noreply.github.com>
2025-07-06 06:15:11 +0700

    Update get.js

--- api/get.js
+++ api/get.js
@@ -2,18 +2,18 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   const { GITHUB_TOKEN, GITHUB_OWNER, GITHUB_REPO } = process.env;
-  const path = 'db.json';
+  const { path } = req.query;
+  if (!path) return res.status(400).json({ error: "Missing path" });
+
   const apiURL = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${path}`;
 
   try {
-    const ghRes = await fetch(apiURL, {
-      headers: { Authorization: `Bearer ${GITHUB_TOKEN}` }
-    });
+    const ghRes = await fetch(apiURL, { headers: { Authorization: `Bearer ${GITHUB_TOKEN}` } });
     const data = await ghRes.json();
     const content = Buffer.from(data.content, 'base64').toString();
     res.status(200).json({ content: JSON.parse(content), sha: data.sha });
-  } catch (e) {
-    console.error(e);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ error: 'Failed to get data' });
   }
 };
